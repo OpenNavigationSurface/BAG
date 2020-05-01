2e065d43a42d559da7635425c108f73f4a9486c8
Colin Wilson <colin.wilson@teledyne.com>
2020-05-01 11:39:14 -0300

    Added more python SurfaceCorrections tests

--- python/test_surfacecorrections.py
+++ python/test_surfacecorrections.py
@@ -118,9 +118,119 @@ def testCreateWriteIrregular():
     del dataset #ensure dataset is deleted before tmpFile
 
 
+def testCreateWriteGridded():
+    tmpFile = testUtils.RandomFileGuard("name")
+    #print(tmpFile.getName())
+
+    dataset = Dataset.create(tmpFile.getName(), Metadata(), chunkSize, compressionLevel)
+    assert(dataset)
+
+    kExpectedSurfaceType = BAG_SURFACE_GRID_EXTENTS
+    kExpectedNumCorrectors = 3
+
+    corrections = dataset.createSurfaceCorrections(kExpectedSurfaceType, 
+        kExpectedNumCorrectors, chunkSize, compressionLevel)
+    assert(corrections)
+
+    descriptor = corrections.getDescriptor()
+    assert(descriptor)
+
+    # Write a record.
+    kExpectedItem0 = BagVerticalDatumCorrectionsGridded((9.87, 6.543, 2.109876))
+
+    kRowStart = 0
+    kColumnStart = 0
+    kRowEnd = 0
+    kColumnEnd = 0
+
+    items = SurfaceCorrectionsGriddedLayerItems((kExpectedItem0,))
+    corrections.write(kRowStart, kColumnStart, kRowEnd, kColumnEnd, items)
+
+    # Read the record back.
+    result = corrections.read(kRowStart, kColumnStart, kRowEnd, kColumnEnd)
+    assert(result)
+
+    correctionsResult = result.asVerticalDatumCorrectionsGridded()
+    assert(len(correctionsResult) == 1)
+
+    actualZValues = correctionsResult[0].zValues()
+    expectedZValues = kExpectedItem0.zValues()
+    
+    assert(all(isclose(actual, expected, abs_tol = 1e-5)
+        for actual, expected in zip(actualZValues, expectedZValues)))
+
+    del dataset #ensure dataset is deleted before tmpFile
+
+
+def testCreateWriteTwoGridded():
+    tmpFile = testUtils.RandomFileGuard("name")
+    #print(tmpFile.getName())
+
+    kRowStart = 0
+    kColumnStart = 0
+    kRowEnd = 0
+    kColumnEnd = 1
+
+    kExpectedNumCorrectors = 3
+
+    kExpectedItems = (BagVerticalDatumCorrectionsGridded((1.23, 4.56, 7.89)),
+                      BagVerticalDatumCorrectionsGridded((9.87, 6.54, 3.21)))
+
+
+    # Write the surface corrections.
+    metadata = Metadata()
+    metadata.loadFromBuffer(bagMetadataSamples.kMetadataXML)
+
+    dataset = Dataset.create(tmpFile.getName(), metadata, chunkSize, compressionLevel)
+    assert(dataset)
+
+    kExpectedSurfaceType = BAG_SURFACE_GRID_EXTENTS
+
+    corrections = dataset.createSurfaceCorrections(kExpectedSurfaceType, 
+        kExpectedNumCorrectors, chunkSize, compressionLevel)
+    assert(corrections)
+
+    descriptor = corrections.getDescriptor()
+    assert(descriptor)
+    assert(descriptor.getNumCorrectors() == kExpectedNumCorrectors)
+    assert(descriptor.getSurfaceType() == kExpectedSurfaceType)
+
+    items = SurfaceCorrectionsGriddedLayerItems(kExpectedItems)
+    corrections.write(kRowStart, kColumnStart, kRowEnd, kColumnEnd, items)
+
+
+    # Read the surface corrections back.
+    dataset = Dataset.openDataset(tmpFile.getName(), BAG_OPEN_READONLY)
+    assert(dataset)
+
+    corrections = dataset.getSurfaceCorrections()
+    assert(corrections)
+
+    result = corrections.read(kRowStart, kColumnStart, kRowEnd, kColumnEnd)
+    assert(result)
+
+    correctionsResult = result.asVerticalDatumCorrectionsGridded()
+    assert(len(correctionsResult) == 2)
+
+    actualZValues0 = correctionsResult[0].zValues()
+    expectedZValues0 = kExpectedItems[0].zValues()
+    
+    assert(all(isclose(actual, expected, abs_tol = 1e-5)
+        for actual, expected in zip(actualZValues0, expectedZValues0)))
+
+    actualZValues1 = correctionsResult[1].zValues()
+    expectedZValues1 = kExpectedItems[1].zValues()
+    
+    assert(all(isclose(actual, expected, abs_tol = 1e-5)
+        for actual, expected in zip(actualZValues1, expectedZValues1)))
+
+    del dataset #ensure dataset is deleted before tmpFile
+
+
 # run the unit test methods
 testReadIrregular()
 testCreateIrregular()
 testCreateGridded()
 testCreateWriteIrregular()
-
+testCreateWriteGridded()
+testCreateWriteTwoGridded()
