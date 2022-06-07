ce697f9b0edc16435afda4f01b8821b21e6401e7
selimnairb <selimnairb@gmail.com>
2022-06-07 15:08:10 -0400

    Add version 3 for Catch3 find_package directive; Re-work test discovery and test runner to expose BAG_SAMPLES_PATH at the top level as catch_discover_tests was not passing EXTRA_ARGS as expected

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,38 +6,24 @@
 #include <catch2/catch_all.hpp>
 #include <H5Cpp.h>
 
-
+/**
+ * BAGS Test runner.
+ * Note: Requires that BAG_SAMPLES_PATH environment variable be set. This will typically
+ * be "examples/sample-data". See 'tests/CMakeLists.txt' for how this is handled in the
+ * the context of CMake.
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main(int argc, const char** argv)
 {
     ::H5::Exception::dontPrint();
 
     Catch::Session session;
 
-    std::string samplesPath;
-
-    using namespace Catch::Clara;
-    auto cli = session.cli() |  // Get Catch's composite command line parser.
-        Opt(samplesPath, "samples path")  // bind variable to a new option, with hint string
-        ["-p"]["--samples-path"]  // short and long option name
-        ("Path to samples to be used for testing.");  // description for help output
-
-    session.cli(cli);
-
     const int returnCode = session.applyCommandLine(argc, argv);
     if(returnCode != 0) // Indicates a command line error
         return returnCode;
-
-    if (samplesPath.empty())
-    {
-        std::cerr << "--samples-path parameter not specified.";
-        return EXIT_FAILURE;
-    }
-
-#ifdef _MSC_VER
-    putenv(std::string{"BAG_SAMPLES_PATH=" + samplesPath}.c_str());
-#else
-    ::setenv("BAG_SAMPLES_PATH", samplesPath.c_str(), 1 /*overwrite*/);
-#endif
 
     const int numFailed = session.run();
 
