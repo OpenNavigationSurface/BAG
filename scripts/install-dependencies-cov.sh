cb9cd4211bded36f535c046308a7a67d0cacbad5
selimnairb <selimnairb@gmail.com>
2022-07-14 16:20:33 -0400

    Fix broken dependency for coverage test

--- scripts/install-dependencies-cov.sh
+++ scripts/install-dependencies-cov.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y llvm clang lcov cmake ninja libhdf5-dev libxml2-dev catch2
+sudo apt-get install -y llvm clang lcov cmake ninja-build libhdf5-dev libxml2-dev catch2
