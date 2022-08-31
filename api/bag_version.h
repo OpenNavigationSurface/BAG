e6f0b665bb1a78cb958b780ad2814b8214adedc6
selimnairb <selimnairb@gmail.com>
2022-08-31 15:56:23 -0400

    Change version number from '2.0.0' to '2.0.1' to avoid possible incompatibility with other implementations of BAG version 2 (e.g., GDAL)

--- api/bag_version.h
+++ api/bag_version.h
@@ -1,11 +1,11 @@
 #ifndef BAG_VERSION_H
 #define BAG_VERSION_H
 
-#define BAG_VERSION         "2.0.0"
+#define BAG_VERSION         "2.0.1"
 #define BAG_VERSION_LENGTH  32      // Reserve 32 bytes of space in the attribute.
 #define BAG_VER_MAJOR       2
 #define BAG_VER_MINOR       0
-#define BAG_VER_REVISION    0
+#define BAG_VER_REVISION    1
 
 #endif  // BAG_VERSION_H
 
