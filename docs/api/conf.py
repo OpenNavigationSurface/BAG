fbcf366548712f572b608cbf99e4dacc1c58e841
Roland Arsenault <roland@ccom.unh.edu>
2022-09-20 10:59:47 -0400

    Update readthedocs config

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -3,6 +3,6 @@ import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-  subprocess.call('doxygen docs_config.dox', shell=True)
+  subprocess.call('cd ../..; doxygen docs/api/docs_config.dox', shell=True)
 
   html_extra_path = ['html']
