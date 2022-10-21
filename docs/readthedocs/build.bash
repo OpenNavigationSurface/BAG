aa68d27c2e7eca4b5fcc602f9cfd573cb857a0fd
selimnairb <selimnairb@gmail.com>
2022-10-21 13:48:09 -0400

    Add debugging information to see why Python API docs are imcomplete

--- docs/readthedocs/build.bash
+++ docs/readthedocs/build.bash
@@ -8,4 +8,6 @@ conda activate readthedocs
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build -S . -DBAG_BUILD_SWIG:BOOL=ON -DBAG_BUILD_PYTHON:BOOL=ON -DBAG_BUILD_DOCS:BOOL=ON
 cmake --build build -v
 mkdir -p _readthedocs/html/
+ls -R build/docs/sphinx
 cp -r build/docs/sphinx/* _readthedocs/html/
+ls -R _readthedocs/html
