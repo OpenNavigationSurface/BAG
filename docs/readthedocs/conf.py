fd1c4d78d3dc925a7c64ba2b30fb1ef9a3ff1ad4
selimnairb <selimnairb@gmail.com>
2022-10-21 11:07:29 -0400

    Initial readthedocs custom build config

--- docs/readthedocs/conf.py
+++
@@ -1,8 +0,0 @@
-import subprocess, os
-
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
-if read_the_docs_build:
-  subprocess.call('cd ../..; doxygen docs/doxygen/docs_config.dox', shell=True)
-
-  html_extra_path = ['build/html']
