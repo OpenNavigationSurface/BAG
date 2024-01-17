384b17e80fb50b16faf72aaae107432534ac34bb
selimnairb <selimnairb@gmail.com>
2024-01-17 11:18:49 -0500

    fuzz: Attempt to simplify fuzzer build linking config

--- fuzzers/build.sh
+++ fuzzers/build.sh
@@ -14,8 +14,6 @@ cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build -S . \
 
 cmake --build build --config Release --target install
 
-export LD_LIBRARY_PATH=/usr/lib:/usr/local/lib:/usr/local/HDF_Group/HDF5/1.14.3/lib
-
 echo "Building bag_read_fuzzer..."
 $CXX $CXXFLAGS \
   -I$SRC_DIR/api \
@@ -24,9 +22,7 @@ $CXX $CXXFLAGS \
   -L/opt/lib/static -lbaglib \
   -L/opt/lib -lhdf5_cpp \
   -L/opt/lib -lhdf5 \
-  -L/opt/lib -lxml2 \
-  -Wl,-Bstatic -lxml2 -lhdf5 -lhdf5_cpp -lbaglib \
-  -Wl,-Bdynamic -ldl -lpthread
+  -L/opt/lib -lxml2
 
 echo "Building bag_extended_fuzzer..."
 $CXX $CXXFLAGS \
@@ -36,8 +32,7 @@ $CXX $CXXFLAGS \
   -L/opt/lib/static -lbaglib \
   -L/opt/lib -lhdf5_cpp \
   -L/opt/lib -lhdf5 \
-  -L/opt/lib -lxml2 \
-  -Wl,-Bstatic -lxml2 -lhdf5 -lhdf5_cpp -lbaglib
+  -L/opt/lib -lxml2
 
 echo "Building seed corpus..."
 zip -j $OUT/bag_extended_fuzzer_seed_corpus.zip $SRC_DIR/examples/sample-data/*.bag
