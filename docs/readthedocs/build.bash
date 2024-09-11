88af6622377cbbd45d9a8c7b5298ea1e57b1cca1
selimnairb <selimnairb@gmail.com>
2024-09-10 20:50:43 -0400

    #112: readthedocs: Update conda invocation, attempt 6

--- docs/readthedocs/build.bash
+++ docs/readthedocs/build.bash
@@ -3,6 +3,7 @@ set -eu -o pipefail
 conda init bash
 source ${HOME}/.bashrc
 conda env create -q --file docs/readthedocs/environment.yml
+conda init
 conda activate readthedocs
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build -S . -DBAG_BUILD_PYTHON:BOOL=ON -DBAG_BUILD_DOCS:BOOL=ON
 cmake --build build -v
