09e17f4fe8cf2a55d58ceb452336fa14d3c09403
selimnairb <selimnairb@gmail.com>
2024-09-10 22:35:55 -0400

    #112: readthedocs: Update conda invocation, attempt 11

--- docs/readthedocs/build.bash
+++ docs/readthedocs/build.bash
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 set -eu -o pipefail
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $(pwd)/miniconda
+source "$(pwd)/miniconda/etc/profile.d/conda.sh"
+hash -r
 conda env create -q --file docs/readthedocs/environment.yml
-conda init bash
-source ${HOME}/.bashrc
 conda activate readthedocs
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B build -S . -DBAG_BUILD_PYTHON:BOOL=ON -DBAG_BUILD_DOCS:BOOL=ON
 cmake --build build -v
