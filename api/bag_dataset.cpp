55903bddf3d948b2c6cc188925450bbdb481e3c5
Anthony Papetti <apapetti@outlook.com>
2024-10-31 12:52:47 -0400

    Added error checking for HDF5 Aborts

--- api/bag_dataset.cpp
+++ api/bag_dataset.cpp
@@ -32,6 +32,7 @@
 #include <regex>
 #include <string>
 #include <memory>
+#include <csignal>
 
 
 namespace BAG {
@@ -1055,6 +1056,11 @@ std::tuple<double, double> Dataset::gridToGeo(
     return {x, y};
 }
 
+void handleAbrt(int signum) {
+    std::cerr << "\nUnrecoverable HDF5 Error ";
+    exit(signum);
+}
+
 //! Read an existing BAG.
 /*!
 \param fileName
@@ -1066,6 +1072,7 @@ void Dataset::readDataset(
     const std::string& fileName,
     OpenMode openMode)
 {
+    signal(SIGABRT, handleAbrt);
     m_pH5file = std::unique_ptr<::H5::H5File, DeleteH5File>(new ::H5::H5File{
         fileName.c_str(),
         (openMode == BAG_OPEN_READONLY) ? H5F_ACC_RDONLY : H5F_ACC_RDWR},
