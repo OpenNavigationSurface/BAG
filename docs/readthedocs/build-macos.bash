ffe1ed0e3ab6e6585847d38f2f4d961520fff1e5
selimnairb <selimnairb@gmail.com>
2024-12-14 13:07:05 -0500

    docs: Update readtheocs build to use Ubuntu 24.04 and Python 3.12

--- docs/readthedocs/build-macos.bash
+++ docs/readthedocs/build-macos.bash
@@ -5,6 +5,7 @@ source "$(pwd)/miniconda/etc/profile.d/conda.sh"
 hash -r
 conda env create -q --file docs/readthedocs/environment-macos.yml
 conda activate readthedocs
+rm -rf doc-build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B doc-build -S . -DPython3_FIND_STRATEGY=LOCATION -DBAG_BUILD_PYTHON:BOOL=ON -DBAG_BUILD_DOCS:BOOL=ON
 cmake --build doc-build -v
 mkdir -p _readthedocs/html/
