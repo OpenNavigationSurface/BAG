a8c78c41ae234bb7930460185b75d8ccbbce9c41
selimnairb <selimnairb@gmail.com>
2026-02-04 16:46:44 -0500

    CI: debug readthedocks

--- docs/readthedocs/build.bash
+++ docs/readthedocs/build.bash
@@ -8,6 +8,8 @@ hash -r
 conda env create -q --file docs/readthedocs/environment.yml
 conda activate readthedocs
 rm -rf doc-build
+echo "Installed conda packages..."
+conda list
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B doc-build -S . \
   -DCMAKE_PREFIX_PATH="${CONDA_PREFIX}" \
   -DPython3_FIND_STRATEGY=LOCATION -DBAG_BUILD_PYTHON:BOOL=ON -DBAG_BUILD_DOCS:BOOL=ON
