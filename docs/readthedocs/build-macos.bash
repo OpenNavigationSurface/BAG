7ad1d559996d3ada6a5dae60218ad02d1435a2d1
selimnairb <selimnairb@gmail.com>
2026-02-04 18:54:46 -0500

    CI: readthedocs: pin icu and libxml2 to pre-C++-20 versions

--- docs/readthedocs/build-macos.bash
+++ docs/readthedocs/build-macos.bash
@@ -8,6 +8,8 @@ hash -r
 conda env create -q --file docs/readthedocs/environment-macos.yml
 conda activate readthedocs
 rm -rf doc-build
+echo "Installed conda packages..."
+conda list
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B doc-build -S . \
   -DPython3_FIND_STRATEGY=LOCATION -DBAG_BUILD_PYTHON:BOOL=ON -DBAG_BUILD_DOCS:BOOL=ON
 cmake --build doc-build -v
