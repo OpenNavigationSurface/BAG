db751ce77447b0414fa0d8f7f16527ea815ab5b7
selimnairb <selimnairb@gmail.com>
2026-02-04 17:46:28 -0500

    CI: readthedocs: pin icu to pre-C++20 version

--- docs/readthedocs/build.bash
+++ docs/readthedocs/build.bash
@@ -7,12 +7,12 @@ source "$(pwd)/miniconda/etc/profile.d/conda.sh"
 hash -r
 conda env create -q --file docs/readthedocs/environment.yml
 conda activate readthedocs
+conda install -y conda-which
+
 rm -rf doc-build
 echo "Installed conda packages..."
 conda list
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -B doc-build -S . \
-  -DCMAKE_PREFIX_PATH="${CONDA_PREFIX}" \
-  -DCMAKE_MODULE_PATH="${CONDA_PREFIX}/lib/cmake" \
   -DPython3_FIND_STRATEGY=LOCATION -DBAG_BUILD_PYTHON:BOOL=ON -DBAG_BUILD_DOCS:BOOL=ON
 cmake --build doc-build -v
 mkdir -p _readthedocs/html/
